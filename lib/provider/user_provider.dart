4fe05e63be00d2477f73063586b3cb673e8d1265
Lực Bùi <lucbui@Luke-MBP.lan>
2022-05-08 14:24:59 +0700

    send image va call agora

---
+++ lib/provider/user_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+import 'package:chat_app/models/user.dart';
+import 'package:chat_app/resources/auth_methods.dart';
+
+class UserProvider with ChangeNotifier {
+  User _user;
+  AuthMethods _authMethods = AuthMethods();
+
+  User get getUser => _user;
+
+  void refreshUser() async {
+    User user = await _authMethods.getUserDetails();
+    _user = user;
+    notifyListeners();
+  }
+}
