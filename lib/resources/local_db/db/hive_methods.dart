a088d88e5abd71eabece084cd56a389f9c5abc3e
Lực Bùi <lucbui@Luke-MBP.lan>
2022-05-08 15:35:06 +0700

    Local database

---
+++ lib/resources/local_db/db/hive_methods.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:chat_app/models/log.dart';
+import 'package:chat_app/resources/local_db/interface/log_interface.dart';
+
+class HiveMethods implements LogInterface {
+  String hive_box = "Call_Logs";
+
+  @override
+  init() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+  }
+
+  @override
+  addLogs(Log log) async {
+    var box = await Hive.openBox(hive_box);
+
+    var logMap = log.toMap(log);
+
+    // box.put("custom_key", logMap);
+    int idOfInput = await box.add(logMap);
+
+    close();
+
+    return idOfInput;
+  }
+
+  updateLogs(int i, Log newLog) async {
+    var box = await Hive.openBox(hive_box);
+
+    var newLogMap = newLog.toMap(newLog);
+
+    box.putAt(i, newLogMap);
+
+    close();
+  }
+
+  @override
+  Future<List<Log>> getLogs() async {
+    var box = await Hive.openBox(hive_box);
+
+    List<Log> logList = [];
+
+    for (int i = 0; i < box.length; i++) {
+      var logMap = box.getAt(i);
+
+      logList.add(Log.fromMap(logMap));
+    }
+    return logList;
+  }
+
+  @override
+  deleteLogs(int logId) async {
+    var box = await Hive.openBox(hive_box);
+
+    await box.deleteAt(logId);
+    // await box.delete(logId);
+  }
+
+  @override
+  close() => Hive.close();
+}
