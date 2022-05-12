f0ae10cf5dddf6a15380285bd7501322cc75d887
Lực Bùi <lucbui@Luke-MBP.lan>
2022-05-12 18:36:25 +0700

    open camera DeepAr

--- lib/configs/api_key.dart
+++ lib/configs/api_key.dart
@@ -1,4 +1,4 @@
 const String APP_ID = "f9a22df2ddd940dc9e9fd5a5f4269441";
 
 const String apiArKey =
-    "8b75594dd5b23e3efde72e1de305a5541a848e980bde9216e3af453b2afdc6933c8c3c641bcfa493";
+    "b5bf12cea1ca0d45c9d08c32da71e2f8936e023095047300a2f8f369f18e2a5b3cc69849502e9cbb";
